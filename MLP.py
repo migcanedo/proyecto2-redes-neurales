b80c14818f107ee6723b064ba26d8d2fa4233ed2
RafuCisneros <rafucho_A_cisneros@hotmail.com>
2019-03-23 21:31:55 -0400

    buscando mejor tasa aprendizaje

--- MLP.py
+++ MLP.py
@@ -10,7 +10,6 @@ def leerTXT(archivoDatos):
     # Leer datos
     with open(archivoDatos, newline='') as csvfile:
         datos = csv.reader(csvfile, delimiter=' ')
-        print
         datosArreglo = []
         for fila in datos:
             datosArreglo.append(fila)
@@ -50,46 +49,75 @@ class Perceptron:
             salida = salida + (float(elemento[i]) * float(self.pesos[i]))
         return salida
 
+    def funcionActivacionSigmoidal(self, elemento):
+        salida = self.pesos[0]
+        for i in range(1,self.cantidadParametros+1):
+            salida = salida + (float(elemento[i]) * float(self.pesos[i]))
+        salida = self.funcionSigmoidal(salida)
+        return salida
+
     def activarNeurona(self, elemento):
         if self.funcionActivacion == "Logistica":
             return self.funcionActivacionLogistica(elemento)
         elif self.funcionActivacion == "Lineal":
             return self.funcionActivacionLineal(elemento)
+        elif self.funcionActivacion == "Sigmoidal":
+            return self.funcionActivacionSigmoidal(elemento)
+        elif self.funcionActivacion == "Signo":
+            return self.clasificarFuncionSigno(elemento)        
 
     def derivadaFuncionLogistica(self, elemento):
         return self.funcionLogistica(elemento) * (1 - self.funcionLogistica(elemento))
 
+    def funcionSigmoidal(self, elemento):
+        return(1/(1+exp(-elemento)))
+
+    def derivadaFuncionSigmoidal(self, elemento):
+        return(self.funcionSigmoidal(elemento)*(1-self.funcionSigmoidal(elemento)))
+
+    def clasificarFuncionSigno(self, elemento):
+        salida = self.pesos[0]
+        for i in range(1,self.cantidadParametros+1):
+            salida = salida + (float(elemento[i]) * float(self.pesos[i]))
+        if salida < 0:
+            return -1
+        else:
+            return 1
+
 # Leemos los datos y separamos las entradas
 entradas, respuestasDeseadas = leerTXT("datos_P2_EM2019_N500.txt")
 data_train, data_test, target_train, target_test = train_test_split(entradas, respuestasDeseadas, test_size = 0.2, shuffle=False)
-print(data_test)
 
 erroresEntrenamiento = []
 erroresPrueba = []
 # Cantidad de Neuronas en la capa oculta
-cantNeuronas = [8]
-for n in cantNeuronas:
+n = 8
+tasasAprendizaje = [0.007]
+mejorError = [1000000,10000000,1000000]
+for tasaAprendizaje in tasasAprendizaje:
     # Arreglo de neuronas para la capa oculta
     capaOculta = []
     for i in range(0,n):
-        capaOculta.append(Perceptron(0.1, 1,"Logistica"))
+        capaOculta.append(Perceptron(tasaAprendizaje, 1,"Sigmoidal"))
     # Creamos neurona de salida con funcion de activacion lineal
-    neuronaSalida = Perceptron(0.1, n, "Lineal")
+    neuronaSalida = Perceptron(tasaAprendizaje, n, "Signo")
     epoca = 0
     # Para graficar
     epocas = []
-    errores = []
+    erroresTrain = []
+    erroresTest = []
 
     # Condicion parada backpropagation
-    while(epoca <= 700):
+    while(epoca <= 200):
         if epoca % 350 == 0:
             print("Epoca: " + str(epoca))
         epoca += 1
         epocas.append(epoca)
-        errorCuadraticoMedio = 0
+        errorAcumulado = 0
 
         # Procesamos las entradas
         for i in range(0,len(data_train)): 
+            # FEED FORWARD ########################
             # Pasamos la entrada a cada neurona de la capa oculta
             salidasCapaOculta = []
             for neurona in capaOculta:
@@ -99,7 +127,9 @@ for n in cantNeuronas:
             # Pasamos las salidas de la capa oculta a la neurona de salida
             salidaFinal = neuronaSalida.activarNeurona(salidasCapaOculta)
             error = float(target_train[i])-float(salidaFinal)
-            errorCuadraticoMedio = float(errorCuadraticoMedio) + (float(error)**2)
+            if error != 0:
+                errorAcumulado += 1
+            # BACKPROPAGATION ###################
             gradienteSalida = error # por la derivada de la funcion linea: 1 
             # Actualizo los pesos de la neurona salida
             pesosViejos = []
@@ -117,44 +147,43 @@ for n in cantNeuronas:
                 estimulo = capaOculta[z].pesos[0]
                 for d in range(1,capaOculta[z].cantidadParametros+1):
                     estimulo = estimulo + (float(data_train[i][d]) * float(capaOculta[z].pesos[d]))
-                gradienteLocal = capaOculta[z].derivadaFuncionLogistica(estimulo) # Derivada Funcion Activacion evaluada en el estimulo
+                gradienteLocal = capaOculta[z].derivadaFuncionSigmoidal(estimulo) # Derivada Funcion Activacion evaluada en el estimulo
                 gradienteLocal = gradienteLocal * sumatoriaGradientesPosteriores
                 # Calculo deltaW y actualizo cada peso
                 for k in range(0, len(capaOculta[z].pesos)):
                     deltaW = (float(capaOculta[z].tasaAprendizaje) * float(gradienteLocal) * float(data_train[i][k]))
                     capaOculta[z].pesos[k] = float(capaOculta[z].pesos[k]) + deltaW
 
+        #Agregamos el error al arreglo para grafitcar
+        erroresTrain.append(errorAcumulado/len(data_train))
 
-        # Finalizar el calculo del error cuadratico medio
-        errorCuadraticoMedio = errorCuadraticoMedio/len(data_train)
-        errores.append(errorCuadraticoMedio)
-    print("Error cuadratico medio del conjunto de entrenamiento con " + str(n) + " Neuronas: " + str(errorCuadraticoMedio) + "")
-    erroresEntrenamiento.append(errorCuadraticoMedio)
+        # Verificacion ###########################################################################################
+        # Procesamos las entradas
+        errorAcumulado = 0
+        for i in range(0,len(data_test)): 
+            # Pasamos la entrada a cada neurona de la capa oculta
+            salidasCapaOculta = []
+            for neurona in capaOculta:
+                salidasCapaOculta.append(neurona.activarNeurona(data_test[i]))
+            # Agregamos el sesgo de entrada a la capa de salida
+            salidasCapaOculta.insert(0,1)
+            # Pasamos las salidas de la capa oculta a la neurona de salida
+            salidaFinal = neuronaSalida.activarNeurona(salidasCapaOculta)
+            if float(target_test[i]) != float(salidaFinal):
+                errorAcumulado += 1
+        erroresTest.append(errorAcumulado/len(data_test))
+
+        if mejorError[0] > errorAcumulado:
+            mejorError = [errorAcumulado, epoca, tasaAprendizaje]
+
+print("Mejor error obtenido " + str(mejorError[0]) + " en la epoca " + str(mejorError[1]) + " y tasa de aprendizaje " + str(mejorError[2]))
 
     # Grafica 
-    plt.plot(epocas,errores)
-    plt.xlabel('Epocas')
-    plt.ylabel('Error Cuadratico Medio')
-    plt.title("Historial de Errores")
-    plt.show()
+    # plt.plot(epocas,erroresTrain, label="Entrenamiento")
+    # plt.plot(epocas,erroresTest, label="Prueba")
+    # plt.xlabel('Epocas')
+    # plt.ylabel('% de elementos mal clasificados')
+    # plt.title("Historial de Errores con tasa de aprendizaje " + str(tasaAprendizaje))
+    # plt.legend()
+    # plt.show()
 
-    # Verificacion ###########################################################################################
-    # Procesamos las entradas
-    errorCuadraticoMedio = 0
-    salidasPrueba = []
-    for i in range(0,len(data_test)): 
-        # Pasamos la entrada a cada neurona de la capa oculta
-        salidasCapaOculta = []
-        for neurona in capaOculta:
-            salidasCapaOculta.append(neurona.activarNeurona(data_test[i]))
-        # Agregamos el sesgo de entrada a la capa de salida
-        salidasCapaOculta.insert(0,1)
-        # Pasamos las salidas de la capa oculta a la neurona de salida
-        salidaFinal = neuronaSalida.activarNeurona(salidasCapaOculta)
-        salidasPrueba.append(salidaFinal)
-        error = float(target_test[i])-float(salidaFinal)
-        errorCuadraticoMedio = float(errorCuadraticoMedio) + (float(error)**2)
-
-    errorCuadraticoMedio = errorCuadraticoMedio/len(data_test)
-    erroresPrueba.append(errorCuadraticoMedio)
-    print("Error cuadratico medio del conjunto de prueba con " + str(n) + " Neuronas: " + str(errorCuadraticoMedio) + "")
