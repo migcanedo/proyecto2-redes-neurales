30eed8e109adb22a4a71e56be947a7571144215d
RafuCisneros <rafucho_A_cisneros@hotmail.com>
2019-05-25 18:18:18 -0400

    segundo intento

--- MLP.py
+++ MLP.py
@@ -16,12 +16,48 @@ def leerTXT(archivoDatos):
     respuestasDeseadas = []
     x = []
     for elemento in datosArreglo:
-        respuestasDeseadas.append(float(elemento[-1]))
+        respuestasDeseada = float(elemento[-1])
+        if respuestasDeseada == 1:
+            respuestasDeseadas.append(1)
+        else:
+            respuestasDeseadas.append(0)
         elemento.pop(-1)
         x.append(elemento)
         # Agregamos el sesgo
         elemento.insert(0,1)
     return x, respuestasDeseadas
+
+def procesarMinMax(datos):
+    nuevosDatos = []
+    columnaProcesadas = []
+    for columna in range(1,len(datos[0])):
+        valores = [float(y[columna]) for y in datos]
+        minimo = min(valores)
+        maximo = max(valores)
+        nuevosValores = [(x-minimo)/(maximo-minimo) for x in valores]
+        columnaProcesadas.append(nuevosValores)
+    for x in range(len(datos)):
+        nuevoDato = [1]
+        for y in columnaProcesadas:
+            nuevoDato.append(y[x])
+        nuevosDatos.append(nuevoDato)
+    return nuevosDatos
+
+def procesarMedVar(datos):
+    nuevosDatos = []
+    columnaProcesadas = []
+    for columna in range(1,len(datos[0])):
+        valores = [float(y[columna]) for y in datos]
+        media = np.mean(valores)
+        varianza = np.std(valores)
+        nuevosValores = [(x-varianza)/media for x in valores]
+        columnaProcesadas.append(nuevosValores)
+    for x in range(len(datos)):
+        nuevoDato = [1]
+        for y in columnaProcesadas:
+            nuevoDato.append(y[x])
+        nuevosDatos.append(nuevoDato)
+    return nuevosDatos    
 
 class Perceptron:
     def __init__(self, tasaAprendizaje, cantidadParametros, funcionActivacion):
@@ -32,6 +68,9 @@ class Perceptron:
         for i in range(0,cantidadParametros+1):
             # Iniciamos los pesos con un numero aleatorio entre -0.5 y 0.5
             self.pesos.append(uniform(-0.5,0.5))
+        self.delta = 0
+        self.salida = 0
+        self.error = 0
 
     def funcionLogistica(self, elemento, alpha=1):
         return 1 / (1 + exp(-alpha*elemento))
@@ -85,22 +124,25 @@ class Perceptron:
             return 1
 
 # Leemos los datos y separamos las entradas para entrenamiento y prueba
-entradas, respuestasDeseadas = leerTXT("datos_P2_EM2019_N1000.txt")
+entradas, respuestasDeseadas = leerTXT("datos_P2_EM2019_N500.txt")
+# entradas = procesarMedVar(entradas)
+# entradas = procesarMinMax(entradas)
 data_train, data_test, target_train, target_test = train_test_split(entradas, respuestasDeseadas, test_size = 0.2, shuffle=False)
 
 erroresEntrenamiento = []
 erroresPrueba = []
 # Cantidad de Neuronas en la capa oculta
 n = 8
-tasasAprendizaje = [0.06]
+tasasAprendizaje = [0.0005]
 mejorError = [1000000,10000000,1000000]     # [Error, epoca, tasa de apredizaje]
 for tasaAprendizaje in tasasAprendizaje:
     # Arreglo de neuronas para la capa oculta
     capaOculta = []
     for i in range(0,n):
         capaOculta.append(Perceptron(tasaAprendizaje, 1,"Sigmoidal"))
-    # Creamos neurona de salida con funcion de activacion Signo
-    neuronaSalida = Perceptron(tasaAprendizaje, n, "Signo")
+    # Creamos una capa de salida con funcion de activacion Sigmoidal
+    capaSalida = [Perceptron(tasaAprendizaje, n, "Sigmoidal"),Perceptron(tasaAprendizaje, n, "Sigmoidal")]
+    neuronaSalida = Perceptron(tasaAprendizaje, n, "Sigmoidal")
     epoca = 0
     # Para graficar
     epocas = []
@@ -108,7 +150,7 @@ for tasaAprendizaje in tasasAprendizaje:
     erroresTest = []
 
     # Condicion parada backpropagation
-    while(epoca <= 200):
+    while(epoca <= 2000):
         if epoca % 350 == 0:
             print("Epoca: " + str(epoca))
         epoca += 1
@@ -121,43 +163,41 @@ for tasaAprendizaje in tasasAprendizaje:
             # Pasamos la entrada a cada neurona de la capa oculta
             salidasCapaOculta = []
             for neurona in capaOculta:
-                salidasCapaOculta.append(neurona.activarNeurona(data_train[i]))
+                salida = neurona.activarNeurona(data_train[i])
+                salidasCapaOculta.append(salida)
+                neurona.salida = salida
             # Agregamos el sesgo de entrada a la capa de salida
             salidasCapaOculta.insert(0,1)
-            # Pasamos las salidas de la capa oculta a la neurona de salida
-            salidaFinal = neuronaSalida.activarNeurona(salidasCapaOculta)
-            error = float(target_train[i])-float(salidaFinal)
-            if error != 0:
-                errorAcumulado += 1
-            # BACKPROPAGATION ###################
-            gradienteSalida = error # por la derivada de la funcion linea: 1 
-            # Actualizo los pesos de la neurona salida
-            pesosViejos = []
-            for k in range(0, len(neuronaSalida.pesos)):
-                deltaW = (float(neuronaSalida.tasaAprendizaje) * gradienteSalida * float(salidasCapaOculta[k]))
-                pesosViejos.append(neuronaSalida.pesos[k])
-                neuronaSalida.pesos[k] = float(neuronaSalida.pesos[k]) + deltaW
+            # Pasamos las salidas de la capa oculta a la capa de salida
+            for neurona in capaSalida:
+                # Calculamos salida
+                salida = neurona.activarNeurona(salidasCapaOculta)
+                neurona.salida = salida
+                # Calculamos error
+                neurona.error = (target_train[i]-neurona.salida)
+                # Calculamos delta
+                neurona.delta = neurona.salida*(1-neurona.salida)*neurona.error
 
-                                        
-            # Actualizo los pesos de las neuronas de la capa oculta
-            for z in range(0, len(capaOculta)):
-                # Calculamos gradienteLocal
-                sumatoriaGradientesPosteriores = float(gradienteSalida) * float(pesosViejos[z])
-                # Calculamos el estimulo recibido por la neurona
-                estimulo = capaOculta[z].pesos[0]
-                for d in range(1,capaOculta[z].cantidadParametros+1):
-                    estimulo = estimulo + (float(data_train[i][d]) * float(capaOculta[z].pesos[d]))
-                gradienteLocal = capaOculta[z].derivadaFuncionSigmoidal(estimulo) # Derivada Funcion Activacion evaluada en el estimulo
-                gradienteLocal = gradienteLocal * sumatoriaGradientesPosteriores
-                # Calculo deltaW y actualizo cada peso
-                for k in range(0, len(capaOculta[z].pesos)):
-                    deltaW = (float(capaOculta[z].tasaAprendizaje) * float(gradienteLocal) * float(data_train[i][k]))
-                    capaOculta[z].pesos[k] = float(capaOculta[z].pesos[k]) + deltaW
+            # Pasamos a la capa oculta 
+            for j in range(len(capaOculta)):
+                # Calculamos el error
+                error = 0
+                for neuronaSalida in capaSalida:
+                    error += neuronaSalida.pesos[j] * neuronaSalida.delta
+                capaOculta[j].error = error
+                # Calculamos el delta
+                capaOculta[j].delta = capaOculta[j].error *  capaOculta[j].salida * (1- capaOculta[j].salida)
 
-        #Agregamos el error al arreglo para grafitcar
-        erroresTrain.append(errorAcumulado/len(data_train))
-
-        # Verificacion ###########################################################################################
+            # Actualizamos los pesos de la capa oculta
+            for neurona in capaOculta:
+                for j in range(0, len(neurona.pesos)):
+                    neurona.pesos[j] += neurona.tasaAprendizaje * neurona.delta * float(data_train[i][j])
+            # Actualizamos los pesos de la capa de salida
+            for neurona in capaSalida:
+                for j in range(0, len(neurona.pesos)):
+                    neurona.pesos[j] += neurona.tasaAprendizaje * neurona.delta * float(salidasCapaOculta[j])
+        
+        # Verificacion con conjunto de prueba #
         # Procesamos las entradas
         errorAcumulado = 0
         for i in range(0,len(data_test)): 
@@ -168,9 +208,46 @@ for tasaAprendizaje in tasasAprendizaje:
             # Agregamos el sesgo de entrada a la capa de salida
             salidasCapaOculta.insert(0,1)
             # Pasamos las salidas de la capa oculta a la neurona de salida
-            salidaFinal = neuronaSalida.activarNeurona(salidasCapaOculta)
-            if float(target_test[i]) != float(salidaFinal):
+            salidaFinal = []
+            for neurona in capaSalida:
+                salidaFinal.append(neurona.activarNeurona(salidasCapaOculta))
+
+        # Verificacion con conjunto de entrenamiento #
+        # Procesamos las entradas
+        errorAcumulado = 0
+        for i in range(0,len(data_train)): 
+            # Pasamos la entrada a cada neurona de la capa oculta
+            salidasCapaOculta = []
+            for neurona in capaOculta:
+                salidasCapaOculta.append(neurona.activarNeurona(data_train[i]))
+            # Agregamos el sesgo de entrada a la capa de salida
+            salidasCapaOculta.insert(0,1)
+            # Pasamos las salidas de la capa oculta a la neurona de salida
+            salidaFinal = []
+            for neurona in capaSalida:
+                salidaFinal.append(neurona.activarNeurona(salidasCapaOculta))
+            if int(target_train[i]) != int(salidaFinal.index(max(salidaFinal))):
                 errorAcumulado += 1
+        
+        erroresTrain.append(errorAcumulado/len(data_train))
+
+        # Verificacion con conjunto de prueba #
+        # Procesamos las entradas
+        errorAcumulado = 0
+        for i in range(0,len(data_test)): 
+            # Pasamos la entrada a cada neurona de la capa oculta
+            salidasCapaOculta = []
+            for neurona in capaOculta:
+                salidasCapaOculta.append(neurona.activarNeurona(data_test[i]))
+            # Agregamos el sesgo de entrada a la capa de salida
+            salidasCapaOculta.insert(0,1)
+            # Pasamos las salidas de la capa oculta a la neurona de salida
+            salidaFinal = []
+            for neurona in capaSalida:
+                salidaFinal.append(neurona.activarNeurona(salidasCapaOculta))
+            if int(target_test[i]) != int(salidaFinal.index(max(salidaFinal))):
+                errorAcumulado += 1
+        
         errorRelativo = errorAcumulado/len(data_test)        
         erroresTest.append(errorRelativo)
 
@@ -183,7 +260,7 @@ for tasaAprendizaje in tasasAprendizaje:
     plt.plot(epocas,erroresTrain, label="Entrenamiento")
     plt.plot(epocas,erroresTest, label="Prueba")
     plt.xlabel('Epocas')
-    plt.ylabel('% de elementos mal clasificados')
+    plt.ylabel('Proporcion de elementos mal clasificados')
     plt.title("Historial de Errores con tasa de aprendizaje " + str(tasaAprendizaje))
     plt.legend()
     plt.show()
